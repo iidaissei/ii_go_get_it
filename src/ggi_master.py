b750181a7077777dee07da2a70d1aa233f59f5c6
iidaissei <issei.288.higashi@icloud.com>
2019-08-11 11:50:11 +0900

    2019/08/11

---
+++ src/ggi_master.py
@@ -0,0 +1,349 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from std_msgs.msg import String, Bool, Float64
+from geometry_msgs.msg import Twist
+from ii_go_get_it.msg import LearnContent
+
+class MimiControlClass():
+    def __init__(self):
+        #Publisher
+        self.cmd_vel_pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist, queue_size = 1)
+        self.m6_pub = rospy.Publisher('/m6_controller/command', Float64, queue_size = 1)
+        self.tts_pub = rospy.Publisher('/tts', String, queue_size = 1)
+
+    def motorControl(self, motor_name, value):
+        if motor_name == 6:
+            m6_angle = Float64()
+            m6_angle = value
+            rospy.sleep(0.1)
+            self.m6_pub.publish(m6_angle)
+            rospy.sleep(1.0)
+
+    def ttsSpeak(self, sentense):
+        data = String()
+        data.data = sentense
+        rospy.sleep(0.1)
+        self.tts_pub.publish(data)
+        rospy.sleep(2.0)
+
+class NavigationClass():
+    def __init__(self):
+        #Publisher
+        self.navigation_memorize_pub = rospy.Publisher('/navigation/memorize_place', String, queue_size = 1)#目的地を記憶
+        self.navigation_command_pub = rospy.Publisher('/navigation/move_place', String, queue_size = 1)#ナビゲーション開始の命令
+        #Subscriber
+        self.navigation_res_sub = rospy.Subscriber('/navigation/result', Bool, self.getNavigationResultCB)
+        
+        self.navigation_result_flg = False
+        self.mimi = MimiControlClass()
+
+    def getNavigationResultCB(self, result_msg):
+        self.navigation_result_flg = result_msg.data
+
+    def setPlace(self, receive_msg): 
+        place_name = String()
+        place_name = receive_msg
+        print place_name
+        rospy.loginfo(" Memorize " + str(place_name.data))
+        self.mimi.ttsSpeak("Memorize " + str(place_name.data))
+        rospy.sleep(0.1)
+        self.navigation_memorize_pub.publish(place_name)
+        while self.navigation_result_flg == False and not rospy.is_shutdown():
+            rospy.sleep(0.5)
+            rospy.loginfo(" Memorizing...")
+        rospy.sleep(0.1)
+        self.navigation_result_flg = False
+        rospy.loginfo(" Memorized " + str(place_name.data))
+        self.mimi.ttsSpeak("I memorized " + str(place_name.data))
+        rospy.sleep(1.0)
+
+    def movePlace(self, receive_msg):
+        self.mimi.motorControl(6, 0.3)
+        place_name = String()
+        place_name.data = receive_msg
+        print place_name
+        rospy.loginfo(" Move to " + str(place_name.data))
+        #self.mimi.ttsSpeak("I move to " + str(place_name.data))
+        rospy.sleep(0.1)
+        self.navigation_command_pub.publish(place_name)
+        while self.navigation_result_flg == False and not rospy.is_shutdown():
+            rospy.sleep(2.5)
+            rospy.loginfo(" Moving...")
+        rospy.sleep(0.1)
+        self.navigation_result_flg = False
+        rospy.loginfo(" Arrived " + str(place_name.data))
+        #self.mimi.ttsSpeak("I arrived " + str(place_name.data))
+        rospy.sleep(1.0)
+
+class ManipulationClass():
+    def __init__(self):
+        #Publisher
+        self.object_grasp_request_pub = rospy.Publisher('/object/grasp_req', String, queue_size = 10)#manipulationの開始
+        self.change_pose_request_pub = rospy.Publisher('/arm/changing_pose_req',String,queue_size=1)
+        #Subscriber
+        self.object_grasp_res_sub = rospy.Subscriber('/object/grasp_res', Bool, self.getObjectGraspResultCB)
+
+        self.object_grasp_result_flg = False    
+        self.object_name = String()
+        self.mimi = MimiControlClass()
+
+    def getObjectGraspResultCB(self, result_msg):
+        self.object_grasp_result_flg = result_msg.data
+
+    def stringMessagePublish(self, target_topic, receive_msg):#String型専用Publisher
+        while not rospy.is_shutdown():
+            request = String()
+            request.data = receive_msg
+            rospy.sleep(0.1)
+            if target_topic == 'object_grasp':
+                self.object_grasp_request_pub.publish(request)
+                break
+            elif target_topic == 'change_pose':
+                self.change_pose_request_pub.publish(request)
+                break
+        rospy.loginfo(" Publeshed " + target_topic + "_request " + receive_msg)
+
+    def grasp(self, receive_msg):
+        while not rospy.is_shutdown():
+            rospy.loginfo("Start grasp")
+            self.mimi.motorControl(6, -0.07)
+            rospy.sleep(0.1)
+            rospy.loginfo("Grasp " + receive_msg)
+            rospy.sleep(0.1)
+            self.stringMessagePublish('object_grasp', 'receive_msg')
+            while not rospy.is_shutdown() and self.object_grasp_result_flg == False:
+                rospy.loginfo(" Waiting for grasping...")
+                rospy.sleep(2.5)
+            self.object_grasp_result_flg = False
+            rospy.loginfo("Grasped " + receive_msg)
+            self.mimi.motorControl(6, 0.3)
+            rospy.loginfo(" Finished grasp")
+            break
+
+    def handOver(self):#objectを手渡す
+        while not rospy.is_shutdown():
+            rospy.loginfo(" Start hand over")
+            self.mimi.motorControl(6, 0.5)
+            self.mimi.ttsSpeak("Here you are")
+            rospy.sleep(0.1)
+            self.stringMessagePublish('change_pose', 'give')
+            rospy.sleep(1.0)
+            self.mimi.motorControl(6, 0.3)
+            rospy.loginfo(" Finished hand over")
+            break
+
+
+class TraningPhase():
+    def __init__(self):
+        #Publisher
+        self.traning_request_pub = rospy.Publisher('/ggi/traning_request', String ,queue_size = 1)#traning用APIの起動・停止
+        self.learn_request_pub = rospy.Publisher('/ggi/learn_request', String, queue_size = 1)#学習用APIの起動・停止
+        self.follow_request_pub = rospy.Publisher('/chase/request', String, queue_size = 1)#followの開始・終了
+        #Subscriber
+        rospy.Subscriber('/ggi/voice_cmd', String, self.getVoiceCmdCB)
+        rospy.Subscriber('/ggi/learn_content', String, self.getLearnContentCB)
+
+        self.voice_cmd = 'Null'
+        self.phase_change_flg = False
+        self.learn_content_flg = False
+        self.mimi = MimiControlClass()
+        self.navi = NavigationClass()
+
+    def getVoiceCmdCB(self, receive_msg):
+        self.voice_cmd = receive_msg.data
+        print self.voice_cmd
+
+    def getLearnContentCB(self, receive_msg):
+        self.place_name = receive_msg.data
+        self.object_name = receive_msg.data
+        self.learn_content_flg = True
+
+    def stringMessagePublish(self, target_topic, receive_msg):#String型専用Publisher
+        while not rospy.is_shutdown():
+            request = String()
+            request.data = receive_msg
+            rospy.sleep(0.1)
+            if target_topic == 'traning':
+                self.traning_request_pub.publish(request)
+                break
+            elif target_topic == 'follow':
+                self.follow_request_pub.publish(request)
+                break
+            elif target_topic == 'learn':
+                self.learn_request_pub.publish(request)
+                break
+        rospy.loginfo(" Publeshed " + target_topic + "_request " + receive_msg)
+
+    def learn(self):#場所のみ記憶させる仕様
+        while not rospy.is_shutdown():
+            self.stringMessagePublish('learn', 'start')#学習用APIの起動
+            while not rospy.is_shutdown() and self.learn_content_flg == False:
+                rospy.loginfo(" Waiting for learning")
+                rospy.sleep(1.0)
+            self.learn_content_flg = False
+            rospy.sleep(1.0)
+            self.navi.setPlace(self.place_name)
+            self.stringMessagePublish('learn', 'stop')#学習用APIの停止
+            rospy.sleep(1.0)
+            break
+
+    def traning(self):
+        try:
+            self.stringMessagePublish('traning', 'start')
+            rospy.sleep(1.0)
+            while not rospy.is_shutdown() and not self.voice_cmd == 'finish_traning':
+                if self.voice_cmd == 'follow_me':#-------------------->follow開始
+                    rospy.sleep(0.5)
+                    self.stringMessagePublish('follow', 'start')
+                    rospy.loginfo(" Start following")
+                    self.mimi.ttsSpeak("I'll follow you")
+                    self.voice_cmd = 'Null'
+                elif self.voice_cmd == 'stop_follow':#--------------->follow終了
+                    self.stringMessagePublish('follow', 'stop')
+                    rospy.loginfo(" Stop following")
+                    self.mimi.ttsSpeak("Stop following")
+                    self.voice_cmd = 'Null'
+                elif self.voice_cmd == 'start_learning':#------------>学習開始
+                    rospy.loginfo(" Start learning")
+                    self.mimi.ttsSpeak("Please tell me")
+                    self.learn()
+                    self.mimi.ttsSpeak("Learning is over")
+                    self.voice_cmd = 'Null'
+                else:
+                    rospy.loginfo(" * Waiting for voice_cmd *")
+                    rospy.sleep(1.0)
+            self.navi.setPlace('operator')#TraningPhaseの最後にオペレーターの場所を記憶
+            self.stringMessagePublish('traning', 'stop')
+            rospy.sleep(0.1)
+            self.phase_change_flg = True
+        except rospy.ROSInterruptException:
+            rospy.loginfo(" Interrupted")
+            pass
+
+    def master(self):
+        try:
+            print '-'*80
+            rospy.loginfo(" Start Go Get It")
+            rospy.sleep(0.3)
+            self.mimi.motorControl(6, 0.3)
+            rospy.sleep(0.5)
+            rospy.loginfo(" Start TraningPhase")
+            self.mimi.ttsSpeak("Start TraningPhase")
+            while not rospy.is_shutdown() and self.phase_change_flg == False:
+                self.traning()
+                rospy.sleep(0.1)
+            rospy.loginfo(" Finished TraningPhase")
+            self.mimi.ttsSpeak("Finished TraningPhase")
+            rospy.sleep(5.0)
+            return 1
+        except rospy.ROSInterruptException:
+            rospy.loginfo(" Interrupted")
+            pass
+
+
+class TestPhase():
+    def __init__(self):
+        #Publisher
+        self.mission_listen_request_pub = rospy.Publisher('/ggi/mission_listen', String, queue_size = 1)
+        #Subscriber
+        rospy.Subscriber('/ggi/order_content', String, self.getOrderContentCB)
+
+        self.order_place = 'Null'
+        self.order_object = 'Null'
+        self.mimi = MimiControlClass()
+        self.navi = NavigationClass()
+        self.mani = ManipulationClass()
+
+    def getOrderContentCB(self, receive_msg):
+        self.order_place = receive_msg.data
+        self.order_object = receive_msg.data
+
+    def stringMessagePublish(self, target_topic, receive_msg):#String型専用Publisher
+        while not rospy.is_shutdown():
+            request = String()
+            request.data = receive_msg
+            rospy.sleep(0.1)
+            if target_topic == 'mission_listen':
+                self.mission_listen_request_pub.publish(request)
+                break
+        rospy.loginfo(" Publeshed " + target_topic + "_request " + receive_msg)
+
+    def listenOrder(self):
+        rospy.loginfo(" Listening order...")
+        self.mimi.motorControl(6, 0.5)
+        #rospy.sleep(0.1)
+        self.mimi.ttsSpeak(" Please give me a mission")
+        self.stringMessagePublish('mission_listen', 'start')
+        while not rospy.is_shutdown() and self.order_place == 'Null':
+            rospy.loginfo(" Waiting for order")
+            rospy.sleep(2.0)
+        print self.order_place
+        print self.order_object
+
+    def missionExecution(self):
+        try:
+            rospy.loginfo(" Start mission")
+            mission_state = 0
+            while not rospy.is_shutdown() and not mission_state == 5:
+                if mission_state == 0:
+                   #self.listenOrder()
+                   mission_state = 1
+                elif mission_state == 1:
+                    self.navi.movePlace(self.order_place)
+                    mission_state = 2
+                elif mission_state == 2:
+                    self.mani.grasp(self.order_object)
+                    mission_state = 3
+                elif mission_state == 3:
+                    self.navi.movePlace('operator')
+                    mission_state = 4
+                elif mission_state == 4:
+                    self.mani.handOver()
+                    mission_state = 5
+            rospy.loginfo(" Finished mission")
+        except rospy.ROSInterruptException:
+            rospy.loginfo(" Interrupted")
+            pass
+
+    def master(self):
+        try:
+            print '-'*80
+            rospy.loginfo(" Start TestPhase")
+            self.mimi.ttsSpeak("Start TestPhase")
+            #API起動・
+            order_count = 1
+            while not rospy.is_shutdown() and not order_count == 4:#３回のチャレンジ
+                print '-'*80
+                rospy.loginfo(" Mission number : " + str(order_count))
+                self.missionExecution()
+                order_count += 1
+            print '-'*80
+            rospy.loginfo(" All mission complete")
+            rospy.sleep(1.0)
+            rospy.loginfo(" Finished TestPhase")
+            self.mimi.ttsSpeak("Finished TestPhase")
+            rospy.sleep(1.0)
+            rospy.loginfo(" Finished Go Get It")
+            self.mimi.ttsSpeak("Finished Go Get It")
+            return 2
+        except rospy.ROSInterruptException:
+            rospy.loginfo(" Interrupted")
+            pass
+
+
+if __name__ == '__main__':
+    rospy.init_node('ggi_master', anonymous = True)
+    try:
+        state = 0
+        traning = TraningPhase()
+        test = TestPhase()
+        while not rospy.is_shutdown() and not state == 2:
+            if state == 0:
+                state = traning.master()
+            elif state == 1:
+                state = test.master()
+    except rospy.ROSInterruptException:
+        rospy.loginfo(" Interrupted")
+        pass
---
+++ src/ggi_navigation.py
@@ -0,0 +1,163 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+import tf 
+import time
+from math import pi
+import actionlib
+from  std_srvs.srv import Empty
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import String, Bool
+from geometry_msgs.msg import Twist, Quaternion
+from tf2_msgs.msg import TFMessage
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+
+class Navigation:
+    def __init__(self):
+        #Publisher
+        self.navigation_result_pub = rospy.Publisher('/navigation/result', Bool, queue_size = 1)
+        #Subscriber
+        rospy.Subscriber('/navigation/memorize_place', String, self.getMemorizePlaceCB)
+        rospy.Subscriber('/navigation/move_place', String, self.getDestinationCB)
+        self.sub_tf  = rospy.Subscriber('/tf', TFMessage, self.getTfCB)
+        rospy.Subscriber('/odom', Odometry, self.getOdomCB)
+        #Service
+        rospy.wait_for_service('move_base/clear_costmaps')
+        self.clear_costmaps = rospy.ServiceProxy('move_base/clear_costmaps', Empty)
+
+        self.location_name = 'Null'
+        self.location_list = []
+        self.location_pose_x = 0
+        self.location_pose_y = 0
+        self.location_pose_z = 0
+        self.location_pose_w = 0
+        self.destination = 'Null'
+        self.sub_tf_flg = False
+        self.sub_odom_flg = False
+
+    def getMemorizePlaceCB(self, receive_msg):
+        self.location_name = receive_msg.data
+
+    def getDestinationCB(self, receive_msg):
+        self.destination = receive_msg.data
+
+    def getTfCB(self, receive_msg):
+        try:
+            if self.sub_tf.transforms[0].header.frame_id == 'odom':
+                self.tf_pose_x = receive_msg.transforms[0].transform.translation.x
+                self.tf_pose_y = receive_msg.transforms[0].transform.translation.y
+            self.sub_tf_flg = True
+        except IndexError:
+            pass
+
+    def getOdomCB(self, receive_msg):#向きのみを購読
+        try:
+            self.odom_pose_w = receive_msg.pose.pose.orientation.w
+            self.odom_pose_z = receive_msg.pose.pose.orientation.z
+            print 'w',self.tf_pose_w
+            print 'z',self.tf_pose_z
+            self.sub_odom_flg = True
+        except IndexError:
+            pass
+
+    def waitTopic(self):#------------------------------------------------------state 0
+        while not rospy.is_shutdown():
+            if self.location_name != 'Null':
+                rospy.loginfo("*Start LocationList setup*")
+                return 1
+            elif self.destination != 'Null':
+                rospy.loginfo("*Start navigation*")
+                return 2
+            else :
+                return 0
+
+    def setLocationList(self):#------------------------------------------------state 1
+        while not rospy.is_shutdown() and self.sub_tf_flg == False:
+            rospy.sleep(0.1)
+        self.location_pose_x = self.tf_pose_x
+        self.location_pose_y = self.tf_pose_y
+        self.location_pose_z = self.odom_pose_z
+        self.location_pose_w = self.odom_pose_w
+        self.location_list.append([self.location_name, self.location_pose_x, self.location_pose_y, self.location_pose_z, self.location_pose_w])
+        rospy.loginfo("Add *" + self.location_name + "* to the LocationList")
+        self.location_name = 'Null'
+        result = Bool()
+        result.data = True
+        self.navigation_result_pub.publish(result)
+        rospy.loginfo("Published result")
+        rospy.sleep(2.0)
+        result = False
+        self.navigation_result_pub.publish(result)
+        return 0
+
+    def navigateToDestination(self):#------------------------------------------state 2
+        location_num = -1
+        for i in range(len(self.location_list)):
+            if self.destination == self.location_list[i][0]:
+                rospy.loginfo("Destination is " + self.destination)
+                location_num = i
+        if location_num == -1:
+            rospy.loginfo("Not found Destination")
+            result = Bool()
+            result.data = False
+            self.navigation_result_pub.publish(result)
+            return 0
+        ac = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        while not ac.wait_for_server(rospy.Duration(5.0)) and not rospy.is_shutdown():
+            rospy.loginfo("Waiting for action client comes up...")
+        rospy.loginfo("The server comes up")
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = 'map'
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = self.location_list[location_num][1]
+        goal.target_pose.pose.position.y = self.location_list[location_num][2]
+        goal.target_pose.pose.orientation.z = self.location_list[location_num][3]
+        goal.target_pose.pose.orientation.w = self.location_list[location_num][4]
+        rospy.sleep(0.5)
+        self.clear_costmaps()
+        rospy.sleep(1.0)
+        ac.send_goal(goal)
+        rospy.loginfo("Sended Goal")
+        while not rospy.is_shutdown():
+            num = ac.get_state()
+            print num
+            if num == 1:
+                rospy.loginfo("Got out of the obstacle")
+                rospy.sleep(2.0)
+            elif num == 3:
+                rospy.loginfo("Goal")
+                self.destination = 'Null'
+                result = Bool()
+                result.data = True
+                self.navigation_result_pub.publish(result)
+                rospy.loginfo("Published result")
+                num = 0
+                rospy.sleep(2.0)
+                result.data = False
+                rospy.sleep(0.1)
+                self.navigation_result_pub.publish(result)
+                return 0
+            elif num == 4:
+                rospy.loginfo("Buried in obstacle")
+                self.clear_costmaps()
+                rospy.loginfo(" Clear Costmaps")
+                rospy.sleep(1.0)
+                return 2
+
+if __name__ == '__main__':
+    rospy.init_node('ggi_navigation', anonymous = True)
+    try:
+        nav = Navigation()
+        state = 0
+        while not rospy.is_shutdown():
+            if state == 0:
+                state = nav.waitTopic()
+            elif state == 1:
+                state = nav.setLocationList()
+            elif state == 2:
+                state = nav.navigateToDestination()
+    except rospy.ROSInterruptException:
+        rospy.loginfo(" Interrupted")
+ 
